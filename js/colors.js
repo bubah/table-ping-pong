1a7c1c86d4159cea1915c592f90b7a53522d88e2
Bubah <bconteh@syr.edu>
2020-09-06 14:43:16 -0400

    Initial project setup

---
+++ js/colors.js
@@ -0,0 +1,3 @@
+const black = "#000"
+const blue = "#0000ff"
+const green = "#009933"
---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+// grab a reference of our "canvas" using its id
+const canvas = document.getElementById('canvas');
+
+/* get a "context". Without "context", we can't draw on canvas */
+const ctx = canvas.getContext('2d');
+
+// render function draws everything on to canvas
+function render() {
+  // draw paddle 1
+  
+  // draw paddle 2
+
+  // draw score on left
+
+  // draw score on right
+
+  // draw net
+
+  // draw ball
+
+}
+    
+render();
