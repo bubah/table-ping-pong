464facad2669ef18595145ba1dfbdd685a669a01
Bubah <bconteh@syr.edu>
2020-09-06 19:05:46 -0400

    Create paddles for user1 and user2

--- js/script.js
+++ js/script.js
@@ -5,6 +5,22 @@ const canvas = document.getElementById('canvas');
 /* get a "context". Without "context", we can't draw on canvas */
 const ctx = canvas.getContext('2d');
 
+const user1 = {
+    width: 5,
+    height: canvas.height/3,
+    x:5,
+    y: canvas.height/3,
+    color: 'white'
+}
+
+const user2 = {
+    width: 5,
+    height: canvas.height/3,
+    x:canvas.width - 10,
+    y: canvas.height/3,
+    color: 'white'
+}
+
 // render function draws everything on to canvas
 function render() {
 
@@ -12,8 +28,12 @@ function render() {
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
     // draw paddle 1
-  
+    ctx.fillStyle = user1.color
+    ctx.fillRect(user1.x, user1.y, user1.width, user1.height)
+    
     // draw paddle 2
+    ctx.fillStyle = user2.color
+    ctx.fillRect(user2.x, user2.y, user2.width, user2.height)
 
     // draw score on left
 
